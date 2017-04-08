f38d76a450d0210ae4cf0f8e043922d25c82a6f8
kittysquee <kittysquee@gmail.com>
2017-04-08 15:18:10 +0100

    Adds flash error messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,11 @@ class User
   attr_accessor :password_confirmation
 
   property :id, Serial
-  property :email, String
+  property :email, String, format: :email_address, required: true, unique: true
 
   property :password_digest, Text
   validates_confirmation_of :password
+  # validates_format_of :email, as: :email_address
 
   def password=(password)
     @password = password
--- spec/features/signup_form_spec.rb
+++ spec/features/signup_form_spec.rb
@@ -8,7 +8,7 @@ feature 'Signup Form' do
   scenario 'Requires matching confirmation password' do
     expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
     expect(current_path).to eq '/users'
-    expect(page).to have_content 'Passwords do not match'
+    expect(page).to have_content 'Password does not match the confirmation'
   end
 
   def sign_up(email: 'freddy@freddy.com',
@@ -19,5 +19,23 @@ feature 'Signup Form' do
     fill_in :password, with: password
     fill_in :password_confirmation, with: password_confirmation
     click_button 'Complete Registration'
+  end
+
+  scenario "I can't sign up without an email address" do
+    expect { sign_up(email: nil) }.not_to change(User, :count)
+    expect(current_path).to eq '/users'
+    expect(page).to have_content 'Email must not be blank'
+  end
+
+  scenario "I can't sign up with an invalid email address" do
+    expect { sign_up(email: "invalid@email") }.to change(User, :count).by 0
+    expect(current_path).to eq '/users'
+    expect(page).to have_content 'Email has an invalid format'
+  end
+
+  scenario "I cna't sign up with an existing email" do
+    sign_up
+    expect{ sign_up }.to_not change(User, :count)
+    expect(page).to have_content('Email is already taken')
   end
 end
